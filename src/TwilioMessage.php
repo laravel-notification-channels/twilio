ed15b6475a6f4c328cecf169fc4241bcb140c1eb
Sky Bit <43996164+skybitbbsr@users.noreply.github.com>
2021-03-02 19:12:56 +0530

    add To property in the twilio message
    add to field in message to override $notifiable phone

--- src/TwilioMessage.php
+++ src/TwilioMessage.php
@@ -17,6 +17,14 @@ abstract class TwilioMessage
      * @var string
      */
     public $from;
+    
+    /**
+     * The phone number the message should be sent to.
+     * This is optional. If available, will override $notifiable phone number
+     *
+     * @var string
+     */
+    public $to;
 
     /**
      * @var null|string
@@ -82,6 +90,29 @@ abstract class TwilioMessage
     public function getFrom(): ?string
     {
         return $this->from;
+    }
+    
+    /**
+     * Set the phone number the message should be sent to.
+     *
+     * @param  string $to
+     * @return $this
+     */
+    public function to(string $to): self
+    {
+        $this->to = $to;
+
+        return $this;
+    }
+
+    /**
+     * Get the to address.
+     *
+     * @return string|null
+     */
+    public function getTo(): ?string
+    {
+        return $this->to;
     }
 
     /**
